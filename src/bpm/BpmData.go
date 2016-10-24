b44084b5378470c5b34447241674aa9190be4996
Brandon Bethke <brandon.bethke@neudesic.com>
2016-10-24 14:27:34 -0700

    split out BpmDependency struct

--- src/bpm/BpmData.go
+++ src/bpm/BpmData.go
@@ -8,6 +8,7 @@ import (
     "strings"
     "bpmerror"
 )
+
 /*
 {
     "name": "example",
@@ -24,32 +25,6 @@ import (
     }
 }
 */
-
-type BpmDependency struct {
-    Commit string `json:"commit"`
-    Url    string `json:"url"`
-}
-
-func (dep *BpmDependency) Validate() (error) {
-    if strings.TrimSpace(dep.Url) == "" {
-        return bpmerror.New(nil, "Error: No url specified")
-    }
-    if strings.TrimSpace(dep.Commit) == "" {
-        return bpmerror.New(nil, "Error: No commit specified")
-    }
-    return nil
-}
-
-func (dep *BpmDependency) Equal(item *BpmDependency) bool {
-    // If the address is the same, then it's equal.
-    if dep == item {
-        return true;
-    }
-    if item.Commit == dep.Commit && item.Url == dep.Url {
-        return true;
-    }
-    return false;
-}
 
 type BpmData struct {
 	Name    string `json:"name"`
